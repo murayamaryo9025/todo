671ea940dbdc52a5af3a72a01811d61c39d12089
murayamaryo9025 <153089990+murayamaryo9025@users.noreply.github.com>
2024-02-19 19:24:31 +0900

    Create AuthorController.php

---
+++ src/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+    public function find()
+    {
+        return view('find', ['input' => '']);
+    }
+    public function search(Request $request)
+    {
+        $item = Author::where('name', $request->input)->first();
+        $param = [
+            'input' => $request->input,
+            'item' => $item
+        ];
+        return view('find', $param);
+    }
+    public function bind(Author $author)
+    {
+        $data = [
+            'item'=>$author,
+        ];
+        return view('author.binds', $data);
+    }
+}
