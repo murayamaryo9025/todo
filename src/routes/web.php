96aea95757d623e9136b27f85e2ff8ffb5778776
murayamaryo9025 <153089990+murayamaryo9025@users.noreply.github.com>
2024-02-19 18:38:18 +0900

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AuthorController;
 
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
 Route::get('/', [ContactController::class, 'index']);
 Route::POST('/contacts/confirm', [ContactController::class, 'confirm']);
  Route::post('/contacts', [ContactController::class, 'store']);
