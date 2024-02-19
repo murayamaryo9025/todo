28d79d1bfb8106ca0608d6a35ae08d7ab9c7bcd0
murayamaryo9025 <153089990+murayamaryo9025@users.noreply.github.com>
2024-02-19 15:27:31 +0900

    Update index.blade.php

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,1 +1,99 @@
+<!DOCTYPE html>
+<html lang="ja">
 
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>FashionablyLate</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <a class="header__logo" href="/">
+        Contact
+      </a>
+    </div>
+  </header>
+
+  <main>
+    <div class="__content">
+      <div class="FashionablyLate__heading">
+        <h2>お問い合わせ</h2>
+      </div>
+      <form class="form" action="contacts/confirm" method="post">
+        @csrf
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お名前</span>
+            <span class="form__label--required">必須</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="text" name="name" placeholder="例:山田" />
+              <input type="text" name="name" placeholder="例:太郎" />
+            </div>
+            <div class="form__error">
+              <!--バリデーション機能を実装したら記述します。-->
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">メールアドレス</span>
+            <span class="form__label--required">必須</span>
+            <input type="text" name="name" placeholder="test@example.com">
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="email" name="email" placeholder="test@example.com" />
+            </div>
+            <div class="form__error">
+              <!--バリデーション機能を実装したら記述します。-->
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">電話番号</span>
+            <span class="form__label--required">必須</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="tel" name="tel" placeholder="09012345678" />
+            </div>
+            <div class="form__error">
+              <!--バリデーション機能を実装したら記述します。-->
+            </div>
+          </div>
+        </div>
+         <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お問い合わせの種類</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--textarea">
+              <textarea name="content" placeholder="選択してください"></textarea>
+            </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お問い合わせ内容</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--textarea">
+              <textarea name="content" placeholder="お問い合わせ内容をご記載ください"></textarea>
+            </div>
+          </div>
+        </div>
+        <div class="form__button">
+          <button class="form__button-submit" type="submit">確認画面</button>
+        </div>
+      </form>
+    </div>
+  </main>
+</body>
+
+</html>
